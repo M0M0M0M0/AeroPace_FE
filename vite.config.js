c437ad06aa94507601806733240c44603965d9e5
quocdang <dangvanquocmomo@gmail.com>
2026-06-18 10:41:47 +0700

    config: update build base path to /AeroPace/

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" ? "/Shoestore/" : "/",
+  base: command === "build" ? "/AeroPace/" : "/",
 }));
